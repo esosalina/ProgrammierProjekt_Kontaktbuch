4ce906452236e94c60739f5628bfd5bdefdbfedc
Naomi Uwensuyi <naomi.uwensuyi@students.bfh.ch>
2025-11-28 12:11:11 +0100

    Read me

--- KontakteSuchen.py
+++ KontakteSuchen.py
@@ -1,23 +1,52 @@
 # Kontakte nach Namen, Telefonnummer oder E-Mail-Adresse finden (auch Teiltreffer). 
 
 def search_contact ():
-
+    """ 
+    Lets the user search by name or user ID
+    """
     try:
-        search = input('\nSuche Kontakt: ').lower() 
-        with open ('kontakte.txt', 'r') as datei:
+        search = input('\nSearch Contact: ').lower().strip() 
 
+        with open ('kontakte.txt', 'r') as datei:
             found = False 
 
-            contact_id  = datei.readline()
-            firstname = datei.readline()
-            lastname = datei.readline()
-            phonenumber = datei.readline()
+            while True:
+                
+                contact_id  = datei.readline().strip()
+
+                if not contact_id:
+                    break
+                
+                firstname = datei.readline().strip()
+                lastname = datei.readline().strip()
+                phonenumber = datei.readline().strip()
 
 #wird nur ausgeführt wenn wir einen Kontakt gefunden haben
-            if(search in firstname.lower() or search in lastname.lower()):
+                if(search in firstname.lower() or search in lastname.lower() or search in phonenumber or search in contact_id):
 
-                if
+                    if not found:
+                        found = True
+                        print('\n=== SEARCH RESULTS  ===')
+                        found = True
+                       
+                        print(f"ID: {contact_id}")
+                        print(f"Firstname: {firstname}")
+                        print(f"Lastname: {lastname}")
+                        print(f"Phonenumber: {phonenumber}")
+
+            if not found:
+                print("No contact found.")
         
     except FileNotFoundError as e:
-        print("Contact not found.")
+        print("File not found.")
+
+def contact_filter():
+    
+    with open("kontakte.txt", "r") as datei:
+        contacts = datei.readlines()
+        return contacts
+
+#Programmstart
+if __name__ == '__main__':
+    search_contact()
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ProgrammierProjekt_Kontaktbuch
+
+1 Einleitung/Projektbeschrieb
+2 Wichtigste Funktionen
+
+Readme: Die Dokumentation behandelt relevante Themen (z. B. Beschreibung der Hauptfunktionen, Projektstruktur, Anleitungen usw.). (Aus dem Bewertungssraster)
